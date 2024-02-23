7b983bce7ebae7f464f3fcfcad4a353fc539aeda
Jaotarzan <joaovssouza59@gmail.com>
2024-02-23 10:37:22 -0300

    aula

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const contador = ref(0)
+
+function decrementar() {
+  if (contador.value > 0) contador.value--;
+}
+</script>
+
+<template>
+  <button @click="contador++">+</button>
+  <button @click="decrementar">-</button>
+  <button @click="contador = 0">0</button>
+  <p>valor do contador: {{ contador }}</p>
+</template>
+
+<style scoped>
+
+</style>
